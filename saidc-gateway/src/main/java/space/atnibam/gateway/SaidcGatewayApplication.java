dd1ee155c3d33e562d838941e4fd8ff13af0e124
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-12 00:04:40 +0800

    build: 初始化项目

---
+++ saidc-gateway/src/main/java/space/atnibam/gateway/SaidcGatewayApplication.java
@@ -0,0 +1,13 @@
+package space.atnibam.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SaidcGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaidcGatewayApplication.class, args);
+    }
+}

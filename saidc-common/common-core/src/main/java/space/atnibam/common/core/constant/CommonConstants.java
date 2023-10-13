bf4bbc93187954f712c4bb6c8aa038d5ce6f5c42
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-13 14:13:18 +0800

    feat(CommonConstants): 新增通用常量类
    新增了通用常量类CommonConstants，该类包含了一些全局使用的常量，如UTF-8字符集，成功和失败的标识码，以及http和https的请求类型。这个更改可以使这些常量在整个项目中被统一管理，提高代码的可维护性。

---
+++ saidc-common/common-core/src/main/java/space/atnibam/common/core/constant/CommonConstants.java
@@ -0,0 +1,34 @@
+package space.atnibam.common.core.constant;
+
+/**
+ * @ClassName: CommonConstants
+ * @Description: 通用的常量
+ * @Author: AtnibamAitay
+ * @CreateTime: 2023/10/13 0013 13:23
+ **/
+public class CommonConstants {
+    /**
+     * UTF-8 字符集
+     */
+    public static final String UTF8 = "UTF-8";
+
+    /**
+     * 成功标记
+     */
+    public static final Integer SUCCESS = 200;
+
+    /**
+     * 失败标记
+     */
+    public static final Integer FAIL = 500;
+
+    /**
+     * http请求
+     */
+    public static final String HTTP = "http://";
+
+    /**
+     * https请求
+     */
+    public static final String HTTPS = "https://";
+}

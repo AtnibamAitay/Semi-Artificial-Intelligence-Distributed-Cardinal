7ad50ea25b6f3d713fc347437ef50e1d71419178
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-15 13:09:01 +0800

    feat(SwaggerConstants): 添加Swagger相关常量定义
    本次提交新增了一个名为 SwaggerConstants 的类，用于存放所有与 Swagger 相关的常量。这些常量包括默认的校验KEY、排除路径、根路径、包名以及在安全模式配置中一些常见的字符串常量（如全局作用域、作用域描述、路径匹配正则表达式和 token 传递方式）。通过将这些值抽取为常量，可以增加代码的可读性和可维护性。

---
+++ saidc-common/common-swagger/src/main/java/space/atnibam/common/swagger/constant/SwaggerConstants.java
@@ -0,0 +1,54 @@
+package space.atnibam.common.swagger.constant;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @ClassName: SwaggerConstants
+ * @Description: 定义Swagger相关的常量
+ * @Author: AtnibamAitay
+ * @CreateTime: 2023-10-15 00:50
+ **/
+public class SwaggerConstants {
+
+    /**
+     * 默认校验KEY
+     */
+    public static final String ACCESS_TOKEN_KEY = "Authorization";
+
+    /**
+     * 默认的排除路径，排除Spring Boot默认的错误处理路径和端点
+     */
+    public static final List<String> DEFAULT_EXCLUDE_PATH = Arrays.asList("/error", "/actuator/**");
+
+    /**
+     * 默认根路径
+     */
+    public static final String DEFAULT_BASE_PATH = "/**";
+
+    /**
+     * 默认包
+     */
+    public static final String BASE_PACKAGE = "space.atnibam";
+
+    /**
+     * 表示全局作用域的字符串常量，用于构建 AuthorizationScope 对象。
+     */
+    public static final String GLOBAL_SCOPE = "global";
+
+    /**
+     * 描述作用域权限的字符串常量，用于构建 AuthorizationScope 对象。
+     */
+    public static final String SCOPE_DESCRIPTION = "accessEverything";
+
+    /**
+     * 正则表达式字符串常量，用于在 SecurityContext 中匹配非 auth 开头的路径。
+     */
+    public static final String PATH_REGEX = "^(?!auth).*$";
+
+    /**
+     * 字符串常量，表示在 Swagger 安全模式配置中 token 的传递方式。
+     */
+    public static final String HEADER_TYPE = "header";
+
+}

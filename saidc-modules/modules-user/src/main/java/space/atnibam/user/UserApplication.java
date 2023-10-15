a6b77ee01b140b4c298538c5134aac543ebe95c5
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-15 13:27:31 +0800

    modify(pom): 添加和更新 Maven 依赖
    本次提交针对多个模块的 pom.xml 文件进行了以下更改：
    - 在 `common-core` 模块中，添加了 `springfox-boot-starter` 依赖。
    - 在 `common-swagger` 模块中，添加了 `spring-boot-starter-web`、`springfox-boot-starter`、`lombok` 和 `spring-boot-configuration-processor` 依赖。
    - 在 `saidc-gateway` 模块中，添加了 `springfox-boot-starter` 依赖。
    - 在 `modules-user` 模块中，添加了 `spring-cloud-starter-bootstrap` 和 `common-swagger` 依赖。
    
    这些更改的目的主要是引入 Swagger 来自动生成 API 文档，并引入 Lombok 来简化 POJO 类。

--- saidc-modules/modules-user/src/main/java/space/atnibam/user/UserApplication.java
+++ saidc-modules/modules-user/src/main/java/space/atnibam/user/UserApplication.java
@@ -1,9 +1,13 @@
-package space.atnibam;
+package space.atnibam.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import space.atnibam.common.swagger.annotation.EnableCustomSwagger;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCustomSwagger
 public class UserApplication {
 
     public static void main(String[] args) {

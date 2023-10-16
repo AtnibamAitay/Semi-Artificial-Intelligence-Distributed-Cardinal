cc10ad3b517e176b18580f079ae849b317e5a36c
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-16 21:47:32 +0800

    feat(mapper): 新增文件映射接口FilesMapper
    本次提交新增了一个名为FilesMapper的接口，该接口继承自BaseMapper。

---
+++ saidc-modules/modules-minio/src/main/java/space/atnibam/minio/mapper/FilesMapper.java
@@ -0,0 +1,13 @@
+package space.atnibam.minio.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import space.atnibam.minio.model.entity.Files;
+
+/**
+ * @ClassName: FilesMapper
+ * @Description: 文件映射接口，继承自BaseMapper
+ * @Author: AtnibamAitay
+ * @CreateTime: 2023-10-16 21:46
+ **/
+public interface FilesMapper extends BaseMapper<Files> {
+}

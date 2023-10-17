85f122ddb209d7b62320f5096440d747b3c2fe44
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-17 23:58:50 +0800

    feat(FileServiceConstants): 添加FileServiceConstants常量类
    本次提交在space.atnibam.minio.constant包下添加了一个名为FileServiceConstants的常量类。这个类里定义了一些文件服务相关的常量，如斜杠分隔符、日期格式、日期分隔符和文件后缀名分隔符。

---
+++ saidc-modules/modules-minio/src/main/java/space/atnibam/minio/constant/FileServiceConstants.java
@@ -0,0 +1,26 @@
+package space.atnibam.minio.constant;
+
+/**
+ * @ClassName: FileServiceConstants
+ * @Description: 文件服务常量类
+ * @Author: AtnibamAitay
+ * @CreateTime: 2023/10/17 23:55
+ **/
+public class FileServiceConstants {
+    /**
+     * 斜杠分隔符
+     */
+    public static final String SLASH_SEPARATOR = "/";
+    /**
+     * 日期格式
+     */
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    /**
+     * 日期分隔符
+     */
+    public static final String DATE_SEPARATOR = "-";
+    /**
+     * 文件后缀名分隔符
+     */
+    public static final String EXTENSION_SEPARATOR = ".";
+}

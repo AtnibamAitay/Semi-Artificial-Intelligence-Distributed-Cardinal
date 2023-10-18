209596c1657757a88418a4fd91d9af864100b729
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-18 20:24:52 +0800

    refactor(core): 重构通用响应实体类 R
    这次提交对通用响应实体类 R 进行了重构。主要更改如下：
    1. 使用 Lombok 提供的 @Data 注解替代手动编写的 getter 和 setter 方法，简化了代码；
    2. 引入了 ResultCode 枚举类，用于统一管理状态码和消息，提高了代码的可维护性；
    3. 在 data 字段上添加了 @JsonInclude(JsonInclude.Include.NON_NULL) 注解，使得在数据为 null 时不返回该字段，优化了接口的返回结果。

--- saidc-common/common-core/src/main/java/space/atnibam/common/core/domain/R.java
+++ saidc-common/common-core/src/main/java/space/atnibam/common/core/domain/R.java
@@ -1,225 +1,150 @@
 package space.atnibam.common.core.domain;
 
-import space.atnibam.common.core.constant.CommonConstants;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import space.atnibam.common.core.enums.ResultCode;
 
 import java.io.Serializable;
 
 /**
  * @ClassName: R
- * @Description: 通用响应实体类，包含了常用的响应方法和状态码，可以携带泛型数据
+ * @Description: 这是一个通用的结果类，用于包装服务层返回给前端的数据。它包含了状态码，消息以及具体的业务数据。
  * @Author: AtnibamAitay
- * @CreateTime: 2023-10-13 14:08
+ * @CreateTime: 2023-10-18 16:26
  **/
+@Data
 public class R<T> implements Serializable {
-    /**
-     * 成功状态码
-     */
-    public static final int SUCCESS = CommonConstants.SUCCESS;
 
     /**
-     * 失败状态码
+     * 状态码
      */
-    public static final int FAIL = CommonConstants.FAIL;
+    private Integer code;
 
     /**
-     * 序列化ID
+     * 消息
      */
-    private static final long serialVersionUID = 1L;
+    private String message;
 
     /**
-     * 响应代码
+     * 具体的业务数据，在序列化为 JSON 时，如果 data 为 null 则忽略该字段
      */
-    private int code;
-
-    /**
-     * 响应信息
-     */
-    private String msg;
-
-    /**
-     * 响应数据
-     */
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
 
     /**
-     * 创建一个表示成功的响应，不带任何数据和信息
-     *
-     * @param <T> 数据类型
-     * @return 包含成功状态码的响应
+     * 默认构造函数
      */
-    public static <T> R<T> ok() {
-        return restResult(null, SUCCESS, null);
+    public R() {
     }
 
     /**
-     * 创建一个表示成功的响应，带有数据
+     * 构造函数
      *
-     * @param data 响应数据
-     * @param <T>  数据类型
-     * @return 包含成功状态码和数据的响应
+     * @param code    状态码
+     * @param message 消息
+     * @param data    具体的业务数据
      */
-    public static <T> R<T> ok(T data) {
-        return restResult(data, SUCCESS, null);
-    }
-
-    /**
-     * 创建一个表示成功的响应，带有数据和消息
-     *
-     * @param data 响应数据
-     * @param msg  响应消息
-     * @param <T>  数据类型
-     * @return 包含成功状态码、数据和消息的响应
-     */
-    public static <T> R<T> ok(T data, String msg) {
-        return restResult(data, SUCCESS, msg);
-    }
-
-    /**
-     * 创建一个表示失败的响应，不带任何数据和信息
-     *
-     * @param <T> 数据类型
-     * @return 包含失败状态码的响应
-     */
-    public static <T> R<T> fail() {
-        return restResult(null, FAIL, null);
-    }
-
-    /**
-     * 创建一个表示失败的响应，带有消息
-     *
-     * @param msg 响应消息
-     * @param <T> 数据类型
-     * @return 包含失败状态码和消息的响应
-     */
-    public static <T> R<T> fail(String msg) {
-        return restResult(null, FAIL, msg);
-    }
-
-    /**
-     * 创建一个表示失败的响应，带有数据
-     *
-     * @param data 响应数据
-     * @param <T>  数据类型
-     * @return 包含失败状态码和数据的响应
-     */
-    public static <T> R<T> fail(T data) {
-        return restResult(data, FAIL, null);
-    }
-
-    /**
-     * 创建一个表示失败的响应，带有数据和消息
-     *
-     * @param data 响应数据
-     * @param msg  响应消息
-     * @param <T>  数据类型
-     * @return 包含失败状态码、数据和消息的响应
-     */
-    public static <T> R<T> fail(T data, String msg) {
-        return restResult(data, FAIL, msg);
-    }
-
-    /**
-     * 创建一个表示失败的响应，带有状态码和消息
-     *
-     * @param code 自定义的错误状态码
-     * @param msg  响应消息
-     * @param <T>  数据类型
-     * @return 包含自定义错误状态码和消息的响应
-     */
-    public static <T> R<T> fail(int code, String msg) {
-        return restResult(null, code, msg);
-    }
-
-    /**
-     * 封装响应结果
-     *
-     * @param data 响应数据
-     * @param code 响应代码
-     * @param msg  响应消息
-     * @param <T>  数据类型
-     * @return 封装后的响应结果
-     */
-    private static <T> R<T> restResult(T data, int code, String msg) {
-        R<T> apiResult = new R<>();
-        apiResult.setCode(code);
-        apiResult.setData(data);
-        apiResult.setMsg(msg);
-        return apiResult;
-    }
-
-    /**
-     * 判断响应是否为错误
-     *
-     * @param ret 响应实体
-     * @param <T> 数据类型
-     * @return 如果是错误则返回true，否则false
-     */
-    public static <T> Boolean isError(R<T> ret) {
-        return !isSuccess(ret);
-    }
-
-    /**
-     * 判断响应是否为成功
-     *
-     * @param ret 响应实体
-     * @param <T> 数据类型
-     * @return 如果是成功则返回true，否则false
-     */
-    public static <T> Boolean isSuccess(R<T> ret) {
-        return R.SUCCESS == ret.getCode();
-    }
-
-    /**
-     * 获取响应代码
-     *
-     * @return 响应代码
-     */
-    public int getCode() {
-        return code;
-    }
-
-    /**
-     * 设置响应代码
-     *
-     * @param code 响应代码
-     */
-    public void setCode(int code) {
+    public R(Integer code, String message, T data) {
         this.code = code;
-    }
-
-    /**
-     * 获取响应消息
-     *
-     * @return 响应消息
-     */
-    public String getMsg() {
-        return msg;
-    }
-
-    /**
-     * 设置响应消息
-     *
-     * @param msg 响应消息
-     */
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    /**
-     * 获取响应数据
-     *
-     * @return 响应数据
-     */
-    public T getData() {
-        return data;
-    }
-
-    /**
-     * 设置响应数据
-     *
-     * @param data 响应数据
-     */
-    public void setData(T data) {
+        this.message = message;
         this.data = data;
+    }
+
+    /**
+     * 构造函数
+     *
+     * @param resultCode 状态对象，包含状态码和消息
+     * @param data       具体的业务数据
+     */
+    public R(ResultCode resultCode, T data) {
+        this.code = resultCode.getCode();
+        this.message = resultCode.getMessage();
+        this.data = data;
+    }
+
+    /**
+     * 创建一个表示成功的 Result 对象
+     *
+     * @return 表示成功的 Result 对象
+     */
+    public static <T> R<T> success() {
+        R r = new R();
+        r.setResultCode(ResultCode.SUCCESS);
+        return r;
+    }
+
+    /**
+     * 创建一个表示成功的 Result 对象，并携带具体的业务数据
+     *
+     * @param data 具体的业务数据
+     * @return 表示成功的 Result 对象
+     */
+    public static <T> R<T> success(T data) {
+        R r = new R();
+        r.setResultCode(ResultCode.SUCCESS);
+        r.setData(data);
+        return r;
+    }
+
+    /**
+     * 创建一个表示成功的 Result 对象，并携带状态对象
+     *
+     * @param resultCode 状态对象，包含状态码和消息
+     * @return 表示成功的 Result 对象
+     */
+    public static <T> R<T> success(ResultCode resultCode) {
+        R r = new R();
+        r.setResultCode(resultCode);
+        return r;
+    }
+
+    /**
+     * 创建一个表示失败的 Result 对象，并携带状态对象
+     *
+     * @param resultCode 状态对象，包含状态码和消息
+     * @return 表示失败的 Result 对象
+     */
+    public static <T> R<T> failure(ResultCode resultCode) {
+        R r = new R();
+        r.setResultCode(resultCode);
+        return r;
+    }
+
+    /**
+     * 创建一个表示失败的 Result 对象，并携带状态对象和具体的业务数据
+     *
+     * @param resultCode 状态对象，包含状态码和消息
+     * @param data       具体的业务数据
+     * @return 表示失败的 Result 对象
+     */
+    public static <T> R<T> failure(ResultCode resultCode, T data) {
+        R r = new R();
+        r.setResultCode(resultCode);
+        r.setData(data);
+        return r;
+    }
+
+    /**
+     * 创建一个表示失败的 Result 对象，并携带状态码和消息
+     *
+     * @param code    状态码
+     * @param message 消息
+     * @return 表示失败的 Result 对象
+     */
+    public static <T> R<T> failure(Integer code, String message) {
+        R r = new R();
+        r.setCode(code);
+        r.setMessage(message);
+        return r;
+    }
+
+    /**
+     * 设置状态码和消息
+     *
+     * @param resultCode 状态对象，包含状态码和消息
+     */
+    public void setResultCode(ResultCode resultCode) {
+        this.setCode(resultCode.getCode());
+        this.setMessage(resultCode.getMessage());
     }
 }

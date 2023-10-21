e1c3f23ffbd39181c63efb2283121fec696b8c4b
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-21 15:37:52 +0800

    feat(RedisConstants): 添加新的Redis常量
    在RedisConstants类中添加了几个新的常量，包括锁的默认过期时间、锁的键前缀、线程池大小、获取互斥锁的超时时间和线程休眠的时间，以便于在项目中进行更精确的控制和优化。

--- saidc-common/common-redis/src/main/java/space/atnibam/common/redis/constant/RedisConstants.java
+++ saidc-common/common-redis/src/main/java/space/atnibam/common/redis/constant/RedisConstants.java
@@ -16,4 +16,29 @@ public class RedisConstants {
      * 默认锁对应的值
      */
     public static final String DEFAULT_LOCK_VALUE = "DEFAULT_SUBMIT_LOCK_VALUE";
+
+    /**
+     * Redis 锁的默认过期时间，单位：秒
+     */
+    public static final Long CACHE_NULL_TTL = 2L;
+
+    /**
+     * Redis 锁的键前缀
+     */
+    public static final String LOCK_KEY = "lock:";
+
+    /**
+     * 线程池大小
+     */
+    public static final int THREAD_POOL_SIZE = 10;
+
+    /**
+     * 获取互斥锁的超时时间（10秒）
+     */
+    public static final long LOCK_TIMEOUT = 10L;
+
+    /**
+     * 线程休眠的时间（50毫秒）
+     */
+    public static final long SLEEP_TIME_IN_MILLIS = 50L;
 }
